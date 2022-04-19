68fd3f5d5e9aa505a7a307ad9980772d86c852c6
Emil Matygullin <seb.94@mail.ru>
2022-04-19 12:38:38 +0300

    Initial Commit

---
+++ TheLightUi/TheLightUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheLightUiApp.swift
+//  TheLightUi
+//
+//  Created by Emil on 19.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct TheLightUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
